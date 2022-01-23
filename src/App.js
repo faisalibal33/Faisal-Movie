41b40e1ed630a712fd32134fb833129945099cdc
faisalibal33 <faisaliqbal030397@gmail.com>
2022-01-24 01:10:49 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,45 +1,31 @@
 import axios from 'axios';
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Component/Navbar/Navbar';
 
 function App() {
   const [apimovie, setApimovie] = useState()
   const [search, setSearch] = useState()
   
-  // useEffect (() => {
-  //   cobaApi ()
-  // }, []);
-
-  // const cobaApi = async () => {
-  //   try{
-  //     const {data} = await axios.get(`http://www.omdbapi.com/?apikey=88bf0a87&s=harry potter`)
-  //     console.log (data)
-  //     setApimovie(data)
-  //   }
-  //   catch (error) {
-  //     console.log(error, 'cobaApi Error')
-  //   }
-  // }
-
   const handle = (e) => {
     setSearch(e.target.value)
   }
   const handleSubmit = async (e) => {
     try {
       e.preventDefault()
-      const {data} = await axios.get(`http://www.omdbapi.com/?apikey=88bf0a87&s=${search}`)
-      console.log(data)
-      setApimovie(data)
+      const response = await axios.get(`http://www.omdbapi.com/?apikey=88bf0a87&s=${search}`)
+      setApimovie(response.data.Search)
     }
     catch (error){
       console.log (error, "submit Error")
     }
   }
   
-  
   return (
     <div className="App">
+      <Navbar />
       <h1> Search Movie </h1> <hr/>
       <form onSubmit={handleSubmit}> 
         <input onChange={handle}/>

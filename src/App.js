486e55100d9e869d0e4b6bee1fa5d1a5619bafc2
faisalibal33 <faisaliqbal030397@gmail.com>
2022-01-29 16:09:14 +0700

    edit transition

--- src/App.js
+++ src/App.js
@@ -31,25 +31,6 @@ function App() {
       <Searchmovie handle={handle} handleSubmit={handleSubmit}/>
       <hr/>
       <Card apimovie={apimovie}/>
-      {/* <div className='container'>
-      <div className='row row-cols-md-4'>
-        {apimovie && apimovie.map((data, index) => {
-          console.log(data)
-          return (
-            <div className='mb-3'>
-              <div key={index} className="card">
-                <img src={data.Poster} className="card-img-top" alt="Poster.."/>
-                <div className="card-body">
-                  <h5 className="card-title">{data.Title}</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">{data.Year}</h6>
-                  <a href="#" class="card-link">See Detail</a>
-                </div>
-              </div>
-              </div>
-          )
-        })}
-        </div>
-        </div> */}
     </div>
   );
 }
